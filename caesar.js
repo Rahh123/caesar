619b5c409e1df9bf851e154d18017dcc29f2755b
olidec <69003898+olidec@users.noreply.github.com>
2023-05-04 22:50:30 +0200

    first commit

---
+++ caesar.js
@@ -0,0 +1,22 @@
+function encryptCaesar() {
+    let p = document.getElementById('ptoc-plain').value;
+    let s = parseInt(document.getElementById('ptoc-shift').value);
+    let o = ''
+    for (let i = 0; i < p.length; i++) {
+        let j = p.charCodeAt(i);
+        if (j >= 65 && j <= 90) {
+            j = (j - 65 + s) % 26 + 65;
+            let c = String.fromCharCode(j);
+            o += c;
+        }
+        else if (j >= 97 && j <= 122) {
+            j = (j-97 + s) % 26 + 97;
+            let c = String.fromCharCode(j);
+            o += c;
+        }
+        else {
+            o += p[i]
+        }
+    } 
+    document.getElementById('ptoc-ciphertext').innerHTML = o;   
+}
